3a0f8379879cedc4401ea6f66316d887e11605f3
Sanjar Ahmadov <sa5640@nyu.edu>
2022-03-31 11:14:21 -0400

    cleanup

--- src/single_gpu_clustering.cu
+++ src/single_gpu_clustering.cu
@@ -1,9 +1,6 @@
 /* 
- * This file contains the code for doing the heat distribution problem. 
- * You do not need to modify anything except starting  gpu_heat_dist() at the bottom
- * of this file.
- * In gpu_heat_dist() you can organize your data structure and the call to your
- * kernel(s), memory allocation, data movement, etc. 
+ *
+ * Docs
  * 
  */
 
@@ -16,12 +13,15 @@
 
 /* To index element (i,j) of a 2D array stored as 1D */
 #define index(i, j, N)  ((i)*(N)) + (j)
+
+/* Config params */
 #define PRINT_LOG 0
 #define PRINT_ANALYSIS 0
+
 /* Define constants */
 #define RANGE 100
 
-/*****************************************************************/
+/**************************** Definitions *************************************/
 
 // Function declarations
 void  seq_clustering(float *, unsigned int, unsigned int, int *, float *);
@@ -33,10 +33,11 @@ float calculate_dist(float *, int, int, int);
 void print_float_matrix(float *, int, int);
 void print_int_matrix(int *, int, int);
 int get_parent(int, int *);
+
 // Kernel functions
 //__global__ void calculateMatrix(float * temp_d, float * playground_d, unsigned int N);
 
-// Helper functions
+/*************************** Helper Functions **************************************/
 void print_float_matrix(float * a, int n, int m){
   for(int i=0; i<n; i++){
     for(int j=0; j<m; j++)
@@ -68,9 +69,7 @@ void load_data(float * dataset, int n, int m) {
 }
 
 
-/*****************************************************************/
-/**** Do NOT CHANGE ANYTHING in main() function ******/
-
+/**************************** main() *************************************/
 int main(int argc, char * argv[])
 {
   //Define variables
@@ -110,38 +109,16 @@ int main(int argc, char * argv[])
   
   type_of_device = atoi(argv[3]);
 
-  //N = (unsigned int) atoi(argv[1]);
-  //iterations = (unsigned int) atoi(argv[2]);
- 
-  
-  /* Dynamically allocate NxN array of floats */
-  /*playground = (float *)calloc(N*N, sizeof(float));
-  if( !playground )
-  {
-   fprintf(stderr, " Cannot allocate the %u x %u array\n", N, N);
-   exit(1);
-  }*/
-  
-  /* Initialize it: calloc already initalized everything to 0 */
-  // Edge elements  initialization
-  /*for(i = 0; i < N; i++)
-    playground[index(0,i,N)] = 100;
-  // FIXME: Why N-1? Shouldnt it be N? There is a post about it in Brightspace which has not been answered yet.
-  // Will leave it as it is
-  for(i = 0; i < N-1; i++)
-    playground[index(N-1,i,N)] = 150;
-  */
   float dendrogram[(n-1)*3];
   int * result;
   result = (int *)calloc(n, sizeof(int));
-  if( !type_of_device ) // The CPU sequential version
-  {  
+  if( !type_of_device ) { 
+    // The CPU sequential version
     start = clock();
     seq_clustering(dataset, n, m, result, dendrogram);    
     end = clock();
-  }
-  else  // The GPU version
-  {
+  } else {
+    // The GPU version
      start = clock();
      end = clock();    
   }
@@ -159,26 +136,22 @@ int main(int argc, char * argv[])
 }
 
 
-/*****************  The CPU sequential version (DO NOT CHANGE THAT) **************/
+/*****************  The CPU sequential version **************/
 void  seq_clustering(float * dataset, unsigned int n, unsigned int m, int* result, float * dendrogram)
 {
   // to measure time taken by a specific part of the code 
   double time_taken;
   clock_t start, end;
-
-  /* Dynamically allocate another array for temp values */
-  /* Dynamically allocate NxN array of floats */
   
-  if( !result )
-  {
+  if( !result ) {
    fprintf(stderr, " Cannot allocate result %u array\n", n);
    exit(1);
   }
+
   for (int i = 0; i < n; i++) result[i] = i;
 
   float* dist_matrix = (float *)calloc(n*n, sizeof(float));
-  if( !dist_matrix )
-  {
+  if( !dist_matrix ) {
    fprintf(stderr, " Cannot allocate dist_matrix %u array\n", n*n);
    exit(1);
   }
@@ -230,11 +203,6 @@ void  seq_clustering(float * dataset, unsigned int n, unsigned int m, int* resul
   }
 
   free(dist_matrix);
-  //num_bytes = N*N*sizeof(float);
-  /* Copy initial array in temp */
-  //memcpy((void *)temp, (void *) playground, num_bytes);
-  /* Move new values into old values */ 
-  //memcpy((void *)playground, (void *) temp, num_bytes);
 }
 
 void calculate_pairwise_dists(float * dataset, int n, int m, float * dist_matrix) {
@@ -302,7 +270,7 @@ void merge_clusters(int * result, int data_point_i, int data_point_j, int dim) {
   result[get_parent(data_point_j, result)] = parent_i;
 } 
 
-/***************** The GPU version: Write your code here *********************/
+/***************** The GPU version *********************/
 /* This function can call one or more kernels if you want ********************/
 /*void  gpu_heat_dist(float * playground, unsigned int N, unsigned int iterations)
 {
