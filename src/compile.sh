08fe775aea6461ffd5a63bff003ac69c647beba8
Sanjar Ahmadov <sa5640@nyu.edu>
2022-04-25 16:44:24 -0400

    WIP

--- src/compile.sh
+++ src/compile.sh
@@ -3,7 +3,7 @@ rm cpu1
 rm cpu2
 rm gpu1
 rm gpu2
-nvcc -o cpu1 sequential_clustering.cu
+gcc -o cpu1 naive_cpu_clustering.c
 g++ -o cpu2 cpu_clustering.cpp
 nvcc -o gpu1 single_gpu_clustering.cu
 nvcc -o gpu2 single_gpu_clustering_sort.cu
