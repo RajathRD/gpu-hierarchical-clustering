6dc084c0e5ae0bc67c0a8fa1e2eb928a29b85055
Sanjar Ahmadov <sa5640@nyu.edu>
2022-04-26 02:34:03 -0400

    WIP

--- collect_experiments_results.py
+++ collect_experiments_results.py
@@ -14,8 +14,9 @@ def print_runtimes(cpu1_runtimes, cpu2_runtimes, gpu_runtimes):
         gpu2_entry = gpu_runtimes[i]
         n = cpu1_entry[0]
         m = cpu1_entry[1]
+        
         if not(cpu1_entry[0] == cpu2_entry[0] and cpu2_entry[0] == gpu2_entry[0] and cpu1_entry[1] == cpu2_entry[1] and cpu2_entry[1] == gpu2_entry[1]):
-            raise Exception("Error")
+            raise Exception("Runtimes have not been passed in correct order in terms of inputs")
 
         cpu1 = "{:.2f}".format(cpu1_entry[2]) if cpu1_entry[2] != "N/A" else "N/A"
         cpu2 = "{:.2f}".format(cpu2_entry[2]) if cpu2_entry[2] != "N/A" else "N/A"
